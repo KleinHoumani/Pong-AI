a7d5af2fced27b100c222e3337fbd90850d71823
Klein Houmani <kleinhoumani@gmail.com>
2022-09-20 21:50:35 -0500

    added paddle controls

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ PADDLE_HEIGHT = 100
 
 class Paddle:
     COLOR = WHITE
+    VELOCITY = 4
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -27,6 +28,13 @@ class Paddle:
 
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VELOCITY
+        else:
+
+            self.y += self.VELOCITY
 
 
 def draw(win, left_paddle, right_paddle):
@@ -38,13 +46,23 @@ def draw(win, left_paddle, right_paddle):
     pygame.display.update()
 
 
+def handle_paddle_movement(pressed_keys, left_paddle, right_paddle):
+    if pressed_keys[pygame.K_w] and left_paddle.y - left_paddle.VELOCITY >= 0:
+        left_paddle.move(up=True)
+    if pressed_keys[pygame.K_s] and left_paddle.y + left_paddle.VELOCITY + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if pressed_keys[pygame.K_UP] and right_paddle.y - right_paddle.VELOCITY >= 0:
+        right_paddle.move(up=True)
+    if pressed_keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VELOCITY + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+
 def main():
     run = True
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
-
 
     while run:
         clock.tick(FPS)
@@ -53,6 +71,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        pressed_keys = pygame.key.get_pressed()
+        handle_paddle_movement(pressed_keys, left_paddle, right_paddle)
 
     pygame.quit()
 
